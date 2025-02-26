ccbeed67d92287049187d60c75491771318eee18
jut <jangutm600.gmail.com>
2025-02-26 22:27:57 +0900

    add : Session 실습 예제

---
+++ src/main/java/com/example/study2/basic1/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.study2.basic1.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.study2.basic1.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
+	boolean existsByEmail(String email);
+	boolean existsByNickname(String nickname);
+	Optional<User> findByUuid(String uuid);
+}
+
