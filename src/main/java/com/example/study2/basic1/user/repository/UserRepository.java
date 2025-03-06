2c3d0fc11b19fdd29308086abaf27ae563551bc3
jut <jangutm600.gmail.com>
2025-03-07 00:17:32 +0900

    add : Cascade , orphanRemoval 실습 예제

--- src/main/java/com/example/study2/basic1/user/repository/UserRepository.java
+++ src/main/java/com/example/study2/basic1/user/repository/UserRepository.java
@@ -11,5 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	boolean existsByEmail(String email);
 	boolean existsByNickname(String nickname);
 	Optional<User> findByUuid(String uuid);
+
 }
 
